880edce12081626045bae167d6977bfbea408cd7
j0selep <153457336+j0selep@users.noreply.github.com>
2024-08-13 17:21:51 -0300

    Add files via upload

---
+++ desafio05.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número para calcular a tabuada: '))
+print(f'A tabuada do {n} é: \n1x{n}={n*1}\n2x{n}={2*n}\n3x{n}={3*n}\n4x{n}={4*n}\n5x{n}={5*n}'
+      f'\n6x{n}={6*n}\n7x{n}={7*n}\n8x{n}={8*n}\n9x{n}={n*9}\n10x{n}={10*n}')
